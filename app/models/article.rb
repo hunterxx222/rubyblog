9f8469eaa76c54d77aa70f506407f1cf6bc0a6cb
Toan Ngo <xhunter9x@gmail.com>
2016-10-03 22:50:52 +0700

    finished main objectives

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,10 @@
 class Article < ApplicationRecord
-	has_many :comments
+	has_many :comments, dependent: :destroy
   	validates :title, presence: true,
                     length: { minimum: 5 }
+
+     def self.search(search)
+	  where("title ILIKE ?", "%#{search}%") 
+	  where("body ILIKE ?", "%#{search}%")
+	end
 end
